0c257e21d140ef273745ecf9a07211287c26321a
xrKrodin <16670637+KRodinn@users.noreply.github.com>
2022-01-11 17:10:17 +0300

    Фикс кривых тхм из кока (но я не уверен что он работает)

--- ogsr_engine/xrCore/FS.h
+++ ogsr_engine/xrCore/FS.h
@@ -188,21 +188,86 @@ public:
 
 	IC	u32 		find_chunk	(u32 ID, BOOL* bCompressed = 0)	
 	{
-		u32	dwSize,dwType;
+		u32 dwSize{}, dwType{};
+		bool success{};
 
-		rewind();
-		while (!eof()) {
+		if (m_last_pos != 0)
+		{
+			impl().seek(m_last_pos);
 			dwType = r_u32();
 			dwSize = r_u32();
-			if ((dwType&(~CFS_CompressMark)) == ID) {
-				
-				VERIFY	((u32)impl().tell() + dwSize <= (u32)impl().length());
-				if (bCompressed) *bCompressed = dwType&CFS_CompressMark;
-				return dwSize;
+			if ((dwType & (~CFS_CompressMark)) == ID)
+			{
+				success = true;
 			}
-			else	impl().advance(dwSize);
 		}
-		return 0;
+		if (!success)
+		{
+			rewind();
+			while (!eof())
+			{
+				dwType = r_u32();
+				dwSize = r_u32();
+				if ((dwType & (~CFS_CompressMark)) == ID)
+				{
+					success = true;
+					break;
+				}
+				else
+				{
+					if ((ID & 0x7ffffff0) == 0x810) // is it a thm chunk ID?
+					{
+						const u32 pos = (u32)impl().tell();
+						const u32 size = (u32)impl().length();
+						u32 length = dwSize;
+
+						if (pos + length != size) // not the last chunk in the file?
+						{
+							bool ok = true;
+							if (pos + length > size - 8) ok = false; // size too large?
+							if (ok)
+							{
+								impl().seek(pos + length);
+								if ((r_u32() & 0x7ffffff0) != 0x810) ok = false; // size too small?
+							}
+							if (!ok) // size incorrect?
+							{
+								length = 0;
+								while (pos + length < size) // find correct size, up to eof
+								{
+									impl().seek(pos + length);
+									if (pos + length <= size - 8 && (r_u32() & 0x7ffffff0) == 0x810) break; // found start of next section
+									length++;
+								}
+								Msg("~~THM chunk [%d] fixed, wrong size = [%d], correct size = [%d]", ID, dwSize, length);
+							}
+						}
+
+						impl().seek(pos); // go back to beginning of chunk
+						dwSize = length; // use correct(ed) size
+					}
+					impl().advance(dwSize);
+				}
+			}
+			if (!success)
+			{
+				m_last_pos = 0;
+				return 0;
+			}
+		}
+		VERIFY((u32)impl().tell() + dwSize <= (u32)impl().length());
+		if (bCompressed) *bCompressed = dwType & CFS_CompressMark;
+
+		const u32 dwPos = (u32)impl().tell();
+		if (dwPos + dwSize < (u32)impl().length())
+		{
+			m_last_pos = dwPos + dwSize;
+		}
+		else
+		{
+			m_last_pos = 0;
+		}
+		return dwSize;
 	}
 	
 	IC	BOOL		r_chunk		(u32 ID, void *dest)	// чтение XR Chunk'ов (4b-ID,4b-size,??b-data)
@@ -223,6 +288,9 @@ public:
 			return TRUE;
 		} else return FALSE;
 	}
+
+private:
+	u32 m_last_pos{};
 };
 
 class XRCORE_API IReader : public IReaderBase<IReader> {
